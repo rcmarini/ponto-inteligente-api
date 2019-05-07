70db03dfd1109a8c958fdbd6a40ba64f05f21e11
rcmarini <ricardocmarini@gmail.com>
2019-05-06 22:35:44 -0300

    Criação do serviço de Empresa com testes Unitarios

---
+++ src/main/java/com/rmarini/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,24 @@
+package com.rmarini.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.rmarini.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaService {
+
+	/**
+	 * Retorna uma empresa dado um CNPJ.
+	 * 
+	 * @param cnpj
+	 * @return Optional<Empresa>
+	 */
+	Optional<Empresa> buscaPorCnpj(String cnpj);
+
+	/**
+	 * Cadastra uma nova empresa na base de dados.
+	 * 
+	 * @param empresa
+	 * @return Empresa.
+	 */
+	Empresa persistir(Empresa empresa);
+}
