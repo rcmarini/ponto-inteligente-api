e40f0e97c4405e8306590866044d0c5771fcad40
rcmarini <ricardocmarini@gmail.com>
2019-05-06 22:20:51 -0300

    Ajustes nos testes unitarios de Criptografia de Senha

--- src/test/java/com/rmarini/pontointeligente/api/utils/PasswordUtilsTest.java
+++ src/test/java/com/rmarini/pontointeligente/api/utils/PasswordUtilsTest.java
@@ -4,15 +4,8 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
 public class PasswordUtilsTest {
 
 	private static final String SENHA = "123456";
